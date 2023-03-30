0c4e762f25bc66d45b9c619a1a368d747d6799b4
ChehaYuri <sacra1364069@gmail.com>
2023-03-30 09:51:02 +0300

    добавил задание 1

--- work4.py
+++ work4.py
@@ -1,10 +1,6 @@
 num = input("Введите целое число: ")
 count = [0] * 10
-if num.isdigit():
-    for digit in num:
-        count[int(digit)] += 1
-
-    for i in range(10):
-        print("Количество цифр", i, "в числе:", count[i])
-else:
-    print('Число должно быть типа int')
+for digit in num:
+    count[int(digit)] += 1
+for i in range(10):
+    print("Количество цифр", i, "в числе:", count[i])
