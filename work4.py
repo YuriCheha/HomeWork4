0a8761b18bcf339078092190904a6574bd475ae4
ChehaYuri <sacra1364069@gmail.com>
2023-03-30 09:58:08 +0300

    добавил задание 2

--- work4.py
+++ work4.py
@@ -1,6 +1,16 @@
+# num = input("Введите целое число: ")
+# count = [0] * 10
+# for digit in num:
+#     count[int(digit)] += 1
+# for i in range(10):
+#     print("Количество цифр", i, "в числе:", count[i])
+
+
 num = input("Введите целое число: ")
-count = [0] * 10
+result = ""
 for digit in num:
-    count[int(digit)] += 1
-for i in range(10):
-    print("Количество цифр", i, "в числе:", count[i])
+    if digit == "0":
+        result += "9"
+    else:
+        result += str(9 - int(digit))
+print("Результат: ", result)
