85c7f63cdcdfbf20621e0b6473cccf0ed9377eb8
ChehaYuri <sacra1364069@gmail.com>
2023-03-31 08:38:15 +0300

    добавил задание 4

--- work4.py
+++ work4.py
@@ -15,9 +15,18 @@
 #         result += str(9 - int(digit))
 # print("Результат: ", result)
 
-import random
-lst = [random.randint(0,10) for i in range(5)]
+# import random
+# lst = [random.randint(0,10) for i in range(5)]
+#
+# result = int("".join(map(str, lst)))
+# print(lst)
+# print(result)
 
-result = int("".join(map(str, lst)))
-print(lst)
-print(result)
+a = int(input("Введите первое число: "))
+b = int(input("Введите второе число: "))
+c = int(input("Введите третье число: "))
+
+if b-a == c-b:
+    print("Числа являются последовательными элементами арифметической последовательности")
+else:
+    print("Числа не являются последовательными элементами арифметической последовательности")
